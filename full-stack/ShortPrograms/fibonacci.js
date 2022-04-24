69b4a333e3b018086e2844dc5936764c3b8b8d52
Meripop <99019669+Meripop@users.noreply.github.com>
2022-04-24 17:05:01 +0200

    Create fibonacci.js
    JS fibonacci

---
+++ full-stack/ShortPrograms/fibonacci.js
@@ -0,0 +1,17 @@
+function fibonacci(n) {
+  var lista = [];
+  if (n === 1) {
+    lista = [0];
+  } else if (n === 2) {
+    lista = [0, 1];
+  } else {
+    lista = [0, 1];
+    for (var i = 2; i < n; i++) {
+      var m = lista[lista.length - 1] + lista[lista.length - 2];
+      lista.push(m);
+
+    }
+  }
+  return lista;
+}
+//lenyege, hogy a lista utolsó két szám összege lesz a lista következő eleme

5d96f97f107c1c36a88a4b0e92cd5ca3767911ec
Meripop <99019669+Meripop@users.noreply.github.com>
2022-04-25 12:06:09 +0200

    Create JS-for-loop.js
    JavaScript for-loop

---
+++ full-stack/JavaScript/JS-for-loop.js
@@ -0,0 +1,8 @@
+//For loopnál potnosan megadhatjuk, hogy meddig ismétlődjön a kód
+
+for(i=0;i<5;i++){
+	//csinald ezt
+}
+i=0: csinal egy "i" nevű változót és kezdő értéket ad neki
+i<5: itt érjen véget az ismétlődés, amikor az i értéke 4 lesz
+i++: merre változzon az i értéke, itt mindig egzet ad hozzá

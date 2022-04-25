e8e71650528e067739d9750d317f916c23ccc64f
Meripop <99019669+Meripop@users.noreply.github.com>
2022-04-25 12:07:58 +0200

    Create JS-while-loop.js
    JavaScript while-loop

---
+++ full-stack/JavaScript/JS-while-loop.js
@@ -0,0 +1,13 @@
+//Amíg a zárójelben lévő feltétel igaz addig újra és újra lefogja futtatni a törzsben megadott kódot
+
+while(){
+//csinald ezt
+}
+
+//amíg n kisebb mint 7, írjon ki egy *-ot és n-hez adjon 1-et
+var n=1;
+while(n<7){
+	print("*");
+	n++;
+}
+//eredmény: ******
